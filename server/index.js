a42987808249d082606f56e2dce2469dc09e157c
Jorge Gomez <jorge.luis-leones@hotmail.com>
2023-01-15 18:16:10 -0500

    update

--- server/index.js
+++ server/index.js
@@ -10,6 +10,11 @@ import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
+/* DATA IMPORTS */
+
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 /* CONFIGURATION*/
 
 dotenv.config();
@@ -38,5 +43,9 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+
+    /*ONLY ADD DATA ONE TIME */
+    //User.insertMany(dataUser);
+
 }).catch((error) => console.log(`${error} did not connect`))
 
