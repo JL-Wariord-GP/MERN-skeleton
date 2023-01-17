ac177eb5d28cd5a7369b630384994a38668be97c
Jorge Gomez <jorge.luis-leones@hotmail.com>
2023-01-16 22:10:03 -0500

    update

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ import salesRoutes from './routes/sales.js';
 /* DATA IMPORTS */
 
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js";
+import Product from "./models/Product.js"
+import ProductStat from "./models/ProductStat.js"
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
 
 /* CONFIGURATION*/
 
@@ -45,6 +47,8 @@ mongoose.connect(process.env.MONGO_URL, {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
 
     /*ONLY ADD DATA ONE TIME */
+    //Product.insertMany(dataProduct);
+    //ProductStat.insertMany(dataProductStat);
     //User.insertMany(dataUser);
 
 }).catch((error) => console.log(`${error} did not connect`))
